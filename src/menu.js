643d64befb791523386610717ce44b84c08fc6c7
samuel <samuelgarciacompanys@gmail.com>
2019-10-28 12:31:59 +0100

    linters

--- src/menu.js
+++ src/menu.js
@@ -2,8 +2,8 @@ const menu = (() => {
 
     const addtag = (select, tag, text) => {
         let h = document.createElement(tag);
-        let t = document.createTextNode(text); 
-        h.appendChild(t); 
+        let t = document.createTextNode(text);
+        h.appendChild(t);
         document.querySelector(select).appendChild(h);
     };
 
@@ -16,7 +16,7 @@ const menu = (() => {
 
     const addimg = (select, url) => {
         let h = document.createElement("IMG");
-        h.setAttribute("src", url); 
+        h.setAttribute("src", url);
         h.setAttribute("width", "100%");
         h.setAttribute("height", "100%");
         h.setAttribute("text-align", "center");
@@ -25,24 +25,24 @@ const menu = (() => {
     }
 
     const render = () => {
-    document.getElementById("content").innerHTML = "";
-    adddiv("menu-row", "#content")
-    adddiv("left-menu1", ".menu-row", "card")
-    addimg(".left-menu1", "./images/dish1.jpeg")
-    addtag(".left-menu1", "P", "Sushi arrangement 45$")
-    adddiv("right-menu1", ".menu-row", "card")
-    addimg(".right-menu1", "./images/dish2.jpg")
-    addtag(".right-menu1", "P", "Sashimi arrangement 65$")
+        document.getElementById("content").innerHTML = "";
+        adddiv("menu-row", "#content")
+        adddiv("left-menu1", ".menu-row", "card")
+        addimg(".left-menu1", "./images/dish1.jpeg")
+        addtag(".left-menu1", "P", "Sushi arrangement 45$")
+        adddiv("right-menu1", ".menu-row", "card")
+        addimg(".right-menu1", "./images/dish2.jpg")
+        addtag(".right-menu1", "P", "Sashimi arrangement 65$")
 
-    adddiv("left-menu2", ".menu-row", "card")
-    addimg(".left-menu2", "./images/dish3.jpg")
-    addtag(".left-menu2", "P", "Authentic kobe beef 215$")
-    adddiv("right-menu2", ".menu-row", "card")
-    addimg(".right-menu2", "./images/dish4.jpg")
-    addtag(".right-menu2", "P", "Ramen bowl 30$")
+        adddiv("left-menu2", ".menu-row", "card")
+        addimg(".left-menu2", "./images/dish3.jpg")
+        addtag(".left-menu2", "P", "Authentic kobe beef 215$")
+        adddiv("right-menu2", ".menu-row", "card")
+        addimg(".right-menu2", "./images/dish4.jpg")
+        addtag(".right-menu2", "P", "Ramen bowl 30$")
     }
 
-    return{render};
+    return { render };
 
 })();
 
